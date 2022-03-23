dbafba7219a2da612a73342d741b9c1da5f70587
Aaron Childress <54868656+achildress83@users.noreply.github.com>
2022-03-23 01:31:04 -0700

    Add files via upload

---
+++ docker/ReadMe.md
@@ -0,0 +1,41 @@
+# Objective
+Write a program that calculates pixel coordinate values 
+for an image that is to be displayed on a two dimensional 
+surface given the dimensions of the image and the corner 
+points of the image as it is to be displayed.
+
+**Output:** Program calculates and returns the x and y coordinates 
+at which to plot each pixel in the input image such that 
+the pixels are evenly spaced within the rectangle defined 
+by the corner points.
+
+## Features
+<ul>
+    <li>Corner points can be provided in any order, 
+    program determines which is the bottom left, top right etc.</li>
+    <li>Uses Flask to return response to POST request containing
+    the inputs in the body of the payload</li>
+    <li>Packaged in Docker container</li>
+</ul>
+
+## Preinstall 
+
+Please make sure you install docker on your OS environment.
+
+## How to run
+
+docker-compose up
+
+## Input structure
+
+<code>{
+                "image_dimensions": (10, 12),
+                "corner_points": [
+                    (1.5, 1.5),
+                    (4.0, 1.5),
+                    (1.5, 8.0),
+                    (4.0, 8.0)
+                ]
+            }</code>
+
+**Note:** If using Postman, input must be lists instead of tuples
---
+++ docker/rectangle.py
@@ -0,0 +1,37 @@
+import numpy as np
+import itertools
+
+class Rectangle():
+
+    def __init__(self, image_dimensions, corner_points):
+        self.size = image_dimensions
+        self.corner_points = corner_points
+        self.rows = image_dimensions[0]
+        self.cols = image_dimensions[1]
+
+    # sort corner_points
+    def sort_corner_points(self):
+        '''input: takes a list of tuples representing the four corners of the rectangle
+            output: returns top right and bottom left coordinates'''
+        corner_points_sorted = sorted(self.corner_points, key=lambda i: (i[0], i[1]))
+        bottom_left = corner_points_sorted[0]
+        top_right = corner_points_sorted[-1]
+        return bottom_left, top_right
+
+    def create_coords(self):
+        '''input: takes output from sort_corner_points (bottom left and top right coordinates)
+            output: returns separate arrays of x and y coordinates
+            for all equally spaced points w/in bounds'''
+        bottom_left, top_right = self.sort_corner_points()
+        x_coords = np.linspace(bottom_left[0], top_right[0], num=self.rows)
+        y_coords = np.linspace(bottom_left[1], top_right[1], num=self.cols)
+        return x_coords, y_coords
+
+    # pass row and col attributes to method (to do)
+    def create_matrix(self):
+        '''input: takes output from create_coords (x_coords and y_coords)
+        output: returns solution (all equally spaced points in rectangle)'''
+        x_coords, y_coords = self.create_coords()
+        output = list(itertools.product(x_coords, y_coords))
+        output = np.array([list(i) for i in output]).reshape((self.rows, self.cols, 2))
+        return output.tolist()
